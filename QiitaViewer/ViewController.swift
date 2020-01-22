8eedb88318a1030b348bdfa32bb126ba02d8555b
ShuEngineer <supercar110shu@gmail.com>
2020-01-22 23:27:42 +0900

    Initial Commit

---
+++ QiitaViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QiitaViewer
+//
+//  Created by 伊藤嵩 on 2020/01/22.
+//  Copyright © 2020 Shu Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
